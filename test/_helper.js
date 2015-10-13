0f204984b6808813f21cf0b6538985e612ee5ddc
Samy Pessé <samypesse@gmail.com>
2015-10-13 16:58:50 +0200

    Adapt tests to work on windows

--- test/_helper.js
+++ test/_helper.js
@@ -1,6 +1,11 @@
 var path = require('path');
+var fs = require('fs-extra');
 var config = require('../lib/config');
 
 // Use tmp folder for testing
-config.setRoot(path.resolve(__dirname, 'gitbook'));
-config.init();
+before(function() {
+    var gitbookFolder = path.resolve(__dirname, '../.tmp');
+    fs.removeSync(gitbookFolder);
+    config.setRoot(gitbookFolder);
+    config.init();
+});
